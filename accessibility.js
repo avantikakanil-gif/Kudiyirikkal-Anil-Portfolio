cee3000742251b165917996292661d5913daab01
avantikakanil@gmail.com <avantikakanil@gmail.com>
2026-07-02 08:09:08 -0500

    completed

--- accessibility.js
+++ accessibility.js
@@ -3,8 +3,8 @@ let darkBtn = document.querySelector(".dark");
 darkBtn.addEventListener("click", function(){
     document.body.classList.toggle("darkmode");
     if (document.body.classList.contains("darkmode")) {
-        darkBtn.innerText = "Dark Mode";
-    } else {
         darkBtn.innerText = "Light Mode";
+    } else {
+        darkBtn.innerText = "Dark Mode";
     }
 });
